4786f958351a0577c281c3312cd399b89ec172c5
NagorniyV <prometey2095@gmail.com>
2025-06-03 22:32:49 +0300

    test

--- src/js/main.js
+++ src/js/main.js
@@ -116,3 +116,52 @@ window.scrollTo({
 });
 });
 
+// ФОРМА ОБРАТНОЙ СВЯЗИ
+
+document.getElementById('callbackForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  
+  // Получаем значения из всех полей
+  const name = document.getElementById('nameInput').value;
+  const phoneNumber = document.getElementById('phoneInput').value;
+  const messageText = document.getElementById('messageInput').value;
+  const responseMessage = document.getElementById('responseMessage');
+  const botToken = '7401776138:AAEIszjxs4_-9alGK01THnbG9VHvAGUrEwA';
+  const adminChatIds = ['398501551'];
+  
+  // Формируем сообщение с новыми данными
+  let telegramMessage = `Новий запит на дзвінок!\n`;
+  if (name) telegramMessage += `Ім'я: ${name}\n`;
+  telegramMessage += `Номер телефону: ${phoneNumber}\n`;
+  if (messageText) telegramMessage += `Повідомлення: ${messageText}`;
+  
+  // Отправка в Telegram
+  const sendPromises = adminChatIds.map(chatId => {
+    return fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: telegramMessage
+      })
+    });
+  });
+  
+  // Обработка результатов
+  Promise.all(sendPromises)
+    .then(responses => Promise.all(responses.map(res => res.json())))
+    .then(data => {
+      responseMessage.textContent = 'Дякуємо! Ми вам зателефонуємо найближчим часом.';
+      responseMessage.style.display = 'block';
+      document.getElementById('callbackForm').reset(); // Очищаем всю форму
+      
+      setTimeout(() => {
+        responseMessage.style.display = 'none';
+      }, 5000);
+    })
+    .catch(error => {
+      responseMessage.textContent = 'Виникла помилка. Спробуйте ще раз пізніше.';
+      responseMessage.style.color = 'red';
+      responseMessage.style.display = 'block';
+    });
+});
